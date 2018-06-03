916a41a7eec617a27c86a7ff06c2e904918fea63
Ariel Zhen <zhenpingping@live.com>
2018-06-03 05:14:09 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word 
+
+  def initialize(word) 
+    @word = word 
+  end 
+
+  def match(possible_anagrams)
+    anagrams = []
+    possible_anagrams = %w(possible_anagrams)
+    possible_anagrams.each do |ele|
+      if ele.split("").sort == @word.sort
+        anagrams << ele 
+      end 
+    end 
+    anagrams 
+  end 
+
+end 
