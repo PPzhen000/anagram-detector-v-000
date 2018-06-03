a0048c0de0e2078a1a78eed5f34b96fe3a965bdc
Ariel Zhen <zhenpingping@live.com>
2018-06-03 05:14:15 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,20 @@
 class Anagram
 
-  attr_accessor :word 
+  attr_accessor :word
 
-  def initialize(word) 
-    @word = word 
-  end 
+  def initialize(word)
+    @word = word
+  end
 
   def match(possible_anagrams)
     anagrams = []
     possible_anagrams = %w(possible_anagrams)
     possible_anagrams.each do |ele|
       if ele.split("").sort == @word.sort
-        anagrams << ele 
-      end 
-    end 
+        anagrams << ele
+      end
+    end
     anagrams 
-  end 
+  end
 
-end 
+end
